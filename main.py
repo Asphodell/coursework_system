37c9a400d24f7fc30d260706be6256dfd21fe8fe
Vladislav <91069241+Asphodell@users.noreply.github.com>
2022-11-21 19:16:48 +0300

    added main(), formalized argparser

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import argparse
 import pathlib
-import LogParser
+import floating_parser
+import floating_statistics
 
-parser = argparse.ArgumentParser()
-parser.add_argument("log_name")
-arg = parser.parse_args()
-path_log = pathlib.Path(arg.log_name)
+default_logs_path = pathlib.Path().cwd() / "my_file.txt"
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-p", "--logs-path",
+                       action="store",
+                       dest="logs_file",
+                       required=True,
+                       default=default_logs_path,
+                       help=f"Full path to the Floating server logs file. Default path {default_logs_path}")
+args = argparser.parse_args()
+
+file_path = args.logs_file
+
+
+def main():
+    lp = floating_parser.LogParser(file_path)
+    df = lp.build_df()
+    st = floating_statistics.Statistics(df)
+    print(lp.build_df())
+    print(st.time_and_user())
+    print(st.total_time())
+
 
 if __name__ == "__main__":
-    lp = LogParser.LogParser(path_log)
-    print(lp.build_df())
+    main()
