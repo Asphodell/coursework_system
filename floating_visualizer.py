6894bfb409bf60d6c972ee31be2eee6832d4ad07
Vladislav <91069241+Asphodell@users.noreply.github.com>
2022-12-10 22:18:21 +0300

    one user = one color in users_graph()

--- floating_visualizer.py
+++ floating_visualizer.py
@@ -9,9 +9,10 @@ from plotly.subplots import make_subplots
 
 
 class Visualizer:
-    def __init__(self, df_t_u, df_t_t):
+    def __init__(self, df_t_u, df_t_t, df_s_s):
         self.df_t_u = df_t_u
         self.df_t_t = df_t_t
+        self.df_s_s = df_s_s
 
     def total_time_bar(self):
         df = self.df_t_t
@@ -22,6 +23,9 @@ class Visualizer:
 
     def users_graph(self):
         df = self.df_t_u
+
+        df1 = df
+        df1 = df1.drop_duplicates(subset='User')
 
         x = []
         y = []
@@ -34,8 +38,47 @@ class Visualizer:
             y.append(str(df['User'].loc[df.index[i]]))
             y.append(str(df['User'].loc[df.index[i]]))
 
-            fig.add_trace(go.Scatter(x=x, y=y))
+            trace = fig.add_trace(go.Scatter(x=x, y=y, name=f'{y[0]}'))
 
             x.clear()
             y.clear()
+
+        colors = ['aqua', 'aquamarine',
+            'beige', 'bisque', 'black', 'blanchedalmond', 'blue',
+            'blueviolet','brown', 'burlywood', 'cadetblue',
+            'chartreuse', 'chocolate', 'coral', 'cornflowerblue',
+            'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan',
+            'darkgoldenrod', 'darkgreen',
+            'darkkhaki', 'darkolivegreen', 'darkorange',
+            'darkorchid', 'darkred', 'darksalmon', 'darkseagreen',
+            'darkslateblue','darkslategray', 'darkslategrey',
+            'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue',
+            'dimgray', 'dimgrey', 'dodgerblue', 'firebrick',
+            'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro',
+            'ghostwhite', 'gold', 'goldenrod', 'gray', 'grey', 'green',
+            'greenyellow', 'honeydew', 'hotpink', 'indigo',
+            'ivory', 'khaki', 'lavender', 'lavenderblush', 'lightyellow', 'lime',
+            'magenta','maroon', 'mistyrose', 'moccasin', 'navajowhite', 'navy',
+            'oldlace', 'olive', 'olivedrab', 'orange', 'orangered',
+            'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise',
+            'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink',
+            'plum', 'powderblue', 'purple', 'red', 'rosybrown',
+            'royalblue', 'rebeccapurple', 'saddlebrown', 'salmon',
+            'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver',
+            'skyblue', 'slateblue', 'slategrey', 'snow',
+            'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato',
+            'turquoise', 'violet', 'wheat', 'white', 'whitesmoke',
+            'yellow']
+
+        for i in range(len(df)):
+            for j in range(len(df1)):
+                curr_name = fig['data'][i]['name']
+                name = str(df1['User'].loc[df1.index[j]])
+                if curr_name == name:
+                    fig['data'][i]['line']['color'] = colors[j]
+
+        fig.update_layout(showlegend=False)
         fig.write_html("users_graph.html")
+
+    def start_stop_graph(self):
+        df = self.df_s_s
